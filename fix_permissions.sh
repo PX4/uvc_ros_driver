47cc4dd5b59169c55cfdef9528a57eac277fb207
Simone Guscetti <simonegu@student.ethz.ch>
2016-07-11 08:30:19 +0200

    Mount usb storage device to /media/camera

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -10,8 +10,19 @@ SUBSYSTEMS==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"$idVendor\
 "
 echo "$string" >> /etc/udev/rules.d/99-uvc.rules
 
+string2="usb-storage vendor=0x$idVendor product=0x$idProduct"
+
+echo "$string2" >> /etc/modules
+
 string2="usbserial vendor=0x$idVendor product=0x$idProduct"
 
 echo "$string2" >> /etc/modules
+
+blkid | grep -v -e 'ext4' -e 'swap'
+echo "Provide UUID of the USB device"
+read uuid
+fstab_str="/dev/disk/by-uuid/$uuid    /media/camera   auto    rw,user,noauto  0       0"
+
+echo "$fstab_str" >> /etc/fstab
 
 echo "Unplug the USB device and plug it back in."
