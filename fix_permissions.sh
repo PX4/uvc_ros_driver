cd964af8ae79526a28815d41e5eef73aac5bc7c7
Lorenz Meier <lorenz@px4.io>
2016-07-12 12:22:11 +0200

    Update fix permissions for current device

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-lsusb
-echo "Provide the vendor ID of the USB device:"
-read idVendor
-echo "Provide the product ID of the USB device"
-read idProduct
+idVendor=04b4
+idProduct=00f8
 string="# UVC cameras
 SUBSYSTEMS==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"$idVendor\", ATTRS{idProduct}==\"$idProduct\", MODE=\"0666\"
 "
 echo "$string" >> /etc/udev/rules.d/99-uvc.rules
-
-string2="usbserial vendor=0x$idVendor product=0x$idProduct"
-
-echo "$string2" >> /etc/modules
 
 echo "Unplug the USB device and plug it back in."
