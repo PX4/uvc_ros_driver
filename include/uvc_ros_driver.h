f5f9655f224e59e233cd35a5035e75bd6c89ee7c
root <michael.pantic@gmail.com>
2016-11-28 17:51:53 +0100

    fix tab

--- include/uvc_ros_driver.h
+++ include/uvc_ros_driver.h
@@ -103,7 +103,7 @@ private:
 	const double acc_scale_factor = 16384.0;
 	const double gyr_scale_factor = 131.0;
 	const double deg2rad = 2 * M_PI / 360.0;
-    const double k_ms_to_sec = 1000000.0;
+	const double k_ms_to_sec = 1000000.0;
 
 	// homography variables
 	std::vector<std::pair<int, int>> homography_mapping_;
